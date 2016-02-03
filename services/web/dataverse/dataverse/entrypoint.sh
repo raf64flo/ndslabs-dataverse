d887906d89f175531979e5c2a1c2c7b1c8ba02a1
Craig Willis <willis8@illinois.edu>
2016-02-03 21:34:01 +0000

    Initial docker-compose implementation

--- services/web/dataverse/dataverse/entrypoint.sh
+++ services/web/dataverse/dataverse/entrypoint.sh
@@ -1,1 +1,40 @@
 #!/bin/bash
+set -e
+
+
+if [ -n "$RSERVE_PORT_6311_TCP_ADDR" ]; then
+	RSERVE_HOST=$RSERVE_PORT_6311_TCP_ADDR
+elif [ -z "$RSERVE_HOST" ]; then
+	RSERVE_HOST="localhost"
+fi
+export RSERVE_HOST
+
+if [ -n "$RSERVE_PORT_6311_TCP_PORT" ]; then
+	RSERVE_PORT=$RSERVE_PORT_6311_TCP_PORT
+elif [ -z "$RSERVE_PORT" ]; then
+	RSERVE_PORT="6311"
+fi
+export RSERVE_PORT
+
+# postgres
+if [ -n "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
+	POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
+elif [ -z "$POSTGRES_HOST" ]; then
+	POSTGRES_HOST="localhost"
+fi
+export POSTGRES_HOST
+
+if [ -n "$POSTGRES_PORT_5432_TCP_PORT" ]; then
+	POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
+else
+	POSTGRES_PORT=5432
+fi 
+export POSTGRES_PORT
+
+cd ~/dvinstall
+./start-dataverse
+
+# For Docker, run glassfish verbose to foreground
+#/usr/local/glassfish4/bin/asadmin stop-domain domain1
+#/usr/local/glassfish4/bin/asadmin start-domain --verbose domain1
+while (ps -ef | grep glassfish | grep -v grep) ; do sleep 5; done
--- services/web/dataverse/dataverse/env.sh
+++ services/web/dataverse/dataverse/env.sh
@@ -1,12 +1,12 @@
 export HOST_DNS_ADDRESS=localhost
 export GLASSFISH_DIRECTORY=/usr/local/glassfish4
 export MAIL_SERVER=smtp.ncsa.illinois.edu
-export POSTGRES_SERVER=localhost
+export POSTGRES_SERVER=172.17.0.55
 export POSTGRES_PORT=5432
 export POSTGRES_DATABASE=dvndb
 export POSTGRES_USER=dvnapp
 export POSTGRES_PASSWORD=secret
-export RSERVE_HOST=localhost
+export RSERVE_HOST=172.17.0.100
 export RSERVE_PORT=6311
 export RSERVE_USER=rserve
 export RSERVE_PASSWORD=rserve
