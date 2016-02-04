9e6f9b7c4f13b1b8268d70d3ba038f69d5ed22f6
Craig Willis <willis8@illinois.edu>
2016-02-04 23:41:58 +0000

    Preliminary Kubernetes implementation

--- services/web/dataverse/dataverse/entrypoint.sh
+++ services/web/dataverse/dataverse/entrypoint.sh
@@ -1,52 +1,82 @@
 #!/bin/bash
 set -e
 
+TIMEOUT=30
 
-if [ -n "$RSERVE_PORT_6311_TCP_ADDR" ]; then
-	RSERVE_HOST=$RSERVE_PORT_6311_TCP_ADDR
+echo "Using Rserve at $RSERVE_SERVICE_HOST:$RSERVE_SERVICE_PORT"
+echo "Using Solr at $SOLR_SERVICE_HOST:$SOLR_SERVICE_PORT"
+echo "Using Postgres at $POSTGRES_SERVICE_HOST:$POSTGRES_SERVICE_PORT"
+
+if [ -n "$RSERVE_SERVICE_HOST" ]; then
+	RSERVE_HOST=$RSERVE_SERVICE_HOST
 elif [ -z "$RSERVE_HOST" ]; then
 	RSERVE_HOST="localhost"
 fi
 export RSERVE_HOST
 
-if [ -n "$RSERVE_PORT_6311_TCP_PORT" ]; then
-	RSERVE_PORT=$RSERVE_PORT_6311_TCP_PORT
+if [ -n "$RSERVE_SERVICE_PORT" ]; then
+	RSERVE_PORT=$RSERVE_SERVICE_PORT
 elif [ -z "$RSERVE_PORT" ]; then
 	RSERVE_PORT="6311"
 fi
 export RSERVE_PORT
 
+
+if ncat $RSERVE_HOST $RSERVE_PORT -w $TIMEOUT --send-only < /dev/null ; then 
+	echo Rserve running; 
+else
+	echo Required service Rserve not running on $RSERVE_HOST $RSERVE_PORT;
+        exit 1 
+fi
+
+
 # postgres
-if [ -n "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
-	POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
+if [ -n "$POSTGRES_SERVICE_HOST" ]; then
+	POSTGRES_HOST=$POSTGRES_SERVICE_HOST
 elif [ -z "$POSTGRES_HOST" ]; then
 	POSTGRES_HOST="localhost"
 fi
 export POSTGRES_HOST
 
-if [ -n "$POSTGRES_PORT_5432_TCP_PORT" ]; then
-	POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
+if [ -n "$POSTGRES_SERVICE_PORT" ]; then
+	POSTGRES_PORT=$POSTGRES_SERVICE_PORT
 else
 	POSTGRES_PORT=5432
 fi 
 export POSTGRES_PORT
 
+if ncat $POSTGRES_HOST $POSTGRES_PORT -w $TIMEOUT --send-only < /dev/null ; then 
+	echo Postgres running; 
+else
+	echo Required service Postgres not running on $POSTGRES_HOST $POSTGRES_PORT;
+        exit 1 
+fi
 
 # solr
-if [ -n "$SOLR_PORT_8983_TCP_ADDR" ]; then
-	SOLR_HOST=$SOLR_PORT_8983_TCP_ADDR
+if [ -n "$SOLR_SERVICE_HOST" ]; then
+	SOLR_HOST=$SOLR_SERVICE_HOST
 elif [ -z "$SOLR_HOST" ]; then
 	SOLR_HOST="localhost"
 fi
 export SOLR_HOST
 
-if [ -n "$SOLR_PORT_8983_TCP_PORT" ]; then
-	SOLR_PORT=$SOLR_PORT_8983_TCP_PORT
+if [ -n "$SOLR_SERVICE_PORT" ]; then
+	SOLR_PORT=$SOLR_SERVICE_PORT
 else
 	SOLR_PORT=8983
 fi 
 export SOLR_PORT
 
+if ncat $SOLR_HOST $SOLR_PORT -w $TIMEOUT --send-only < /dev/null ; then 
+	echo Solr running; 
+else
+	echo Required service Solr not running on $SOLR_HOST $SOLR_PORT;
+        exit 1 
+fi
+
+echo "Using Rserve at $RSERVE_HOST:$RSERVE_PORT"
+echo "Using Solr at $SOLR_HOST:$SOLR_PORT"
+echo "Using Postgres at $POSTGRES_HOST:$POSTGRES_PORT"
 
 cd ~/dvinstall
 ./start-dataverse
---
+++ services/web/dataverse/tworavens/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+TIMEOUT=30
+
+if [ -n "$DATAVERSE_SERVICE_HOST" ]; then
+        DATAVERSE_HOST=$DATAVERSE_SERVICE_HOST
+else
+        DATAVERSE_HOST="localhost"
+fi
+
+if [ -n "$DATAVERSE_SERVICE_PORT" ]; then
+        DATAVERSE_PORT=$DATAVERSE_SERVICE_PORT
+else
+        DATAVERSE_PORT=8080
+fi
+
+if [ -n "$TWORAVENS_SERVICE_HOST" ]; then
+        TWORAVENS_HOST=$TWORAVENS_SERVICE_HOST
+else
+        TWORAVENS_HOST="localhost"
+fi
+
+DATAVERSE_URL="$DATAVERSE_HOST:$DATAVERSE_PORT"
+export DATAVERSE_URL;
+
+TWORAVENS_URL="$TWORAVENS_HOST:80"
+export TWORAVENS_URL;
+
+
+echo $DATAVERSE_URL
+echo $TWORAVENS_URL
+
+if ncat $DATAVERSE_HOST $DATAVERSE_PORT -w $TIMEOUT --send-only < /dev/null ; then
+        echo DataVerse running;
+	curl -X PUT -d http://$DATAVERSE_URL/dataexplore/gui.html http://$TWORAVENS_URL/api/admin/settings/:TwoRavensUrl
+else
+        echo Unable to register TwoRavens through DataVerse API
+	echo "curl -X PUT -d http://$DATAVERSE_URL/dataexplore/gui.html http://$TWORAVENS_URL/api/admin/settings/:TwoRavensUrl"
+fi
+
+# Configure TwoRavens to use DataVerse (and DataVerse to use TwoRavens)
+#my $DATAVERSE_URL = $ENV{'DATAVERSE_URL'};
+#my $RAPACHEURL = $ENV{'TWORAVENS_URL'};
+/start-tworavens
+httpd -DFOREGROUND
