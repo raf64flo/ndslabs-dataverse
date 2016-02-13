8b6f7d4bb3db5c8d48b9fc918f45aceb92fc5f90
CoreOS Admin <core@willis8-nds-dev-2.os.ncsa.edu>
2016-02-13 14:40:08 +0000

    NDS-103: Added basic creation dependency handling.

--- services/web/dataverse/start-dataverse.sh
+++ services/web/dataverse/start-dataverse.sh
@@ -25,9 +25,9 @@ start_service_wait ()
    while [ "$status" != 'Running' ]; do
        echo "Waiting for $1 ($status)"
        status=`kubectl get pods | grep $1 | awk '{print $3}'`
-       sleep 5
+       sleep 10 
        ((i++))
-       if [ $i == 5 ]; then
+       if [ $i == 30 ]; then
            echo "Problem starting $1"
            exit 1
        fi
@@ -51,23 +51,10 @@ kubectl get pods
 kubectl get rc
 kubectl get services
 
-#kubectl create -f services/solr-svc.yaml
-#kubectl create -f services/postgres-svc.yaml
-#kubectl create -f services/rserve-svc.yaml
-#kubectl create -f services/dataverse-svc.yaml
-#kubectl create -f services/tworavens-svc.yaml
-
-#kubectl create -f controllers/rserve-rc.yaml
-#kubectl create -f controllers/solr-rc.yaml
-#kubectl create -f controllers/postgres-rc.yaml
-#kubectl create -f controllers/dataverse-rc.yaml
-#kubectl create -f controllers/tworavens-rc.yaml
-
-
 
 dataversePort=`kubectl describe service dataverse  | grep "^NodePort" | awk '{print $3}' | cut -f1 -d"/"`
 twoRavensPort=`kubectl describe service tworavens  | grep "^NodePort" | awk '{print $3}' | cut -f1 -d"/"`
 echo "It will take ~1 minute for DataVerse to start, "
 echo "Then you should be able to access your DataVerse instance on <Node IP>:$dataversePort"
 
-kubectl logs `kubectl get pods | grep tworavens | cut -f1 -d' '`
+kubectl logs `kubectl get pods | grep "^dataverse" | cut -f1 -d' '`
