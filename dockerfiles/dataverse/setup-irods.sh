e9da855199d1e502151c347b011fdf90412558af
Craig Willis <willis8@illinois.edu>
2016-03-25 12:21:34 -0500

    Changed to use shared config from dvicat

--- dockerfiles/dataverse/setup-irods.sh
+++ dockerfiles/dataverse/setup-irods.sh
@@ -5,12 +5,12 @@ cat << EOF > ~/.irods/irods_environment.json
 {
     "irods_host": "$DVICAT_PORT_1247_TCP_ADDR",
     "irods_port": $DVICAT_PORT_1247_TCP_PORT,
-    "irods_user_name": "$IRODS_USER",
-    "irods_zone_name": "$IRODS_ZONE"
+    "irods_user_name": "$PRESERVATION_USER",
+    "irods_zone_name": "$RODS_ZONE"
 }
 EOF
 
-iinit $IRODS_PASSWORD
+iinit $PRESERVATION_PASSWORD
 
 /usr/sbin/crond
 echo "irsync -r /usr/local/glassfish4/glassfish/domains/domain1/files/ i:dvn_preservation/" > /irsync.sh
