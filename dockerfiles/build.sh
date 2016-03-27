4c7b603483526a837ac8389db1a44f22248127df
Craig Willis <willis8@illinois.edu>
2016-03-27 15:59:46 -0500

    Updated version to latest

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,4 +1,5 @@
-docker build -t ndslabs/dataverse:4.2.3 -f dataverse/Dockerfile dataverse
+docker build -t ndslabs/dataverse:latest -f dataverse/Dockerfile dataverse
 docker build -t ndslabs/dataverse-solr:latest -f solr/Dockerfile solr
 docker build -t ndslabs/dataverse-rserve:latest -f rserve/Dockerfile rserve
 docker build -t ndslabs/tworavens:latest -f tworavens/Dockerfile tworavens
+docker build -t ndslabs/dataverse-icat:latest -f icat/Dockerfile icat
